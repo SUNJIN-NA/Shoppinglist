aa32fc5424bbb0dfe647b97d2e46e9f641e36e01
NASUNJIN <sj010618@gmail.com>
2022-11-05 21:25:00 +0900

    update

--- test/add-to-cart.test.js
+++ test/add-to-cart.test.js
@@ -1,8 +1,19 @@
+var request = require('request')
+var expect = require('chai').expect
 var assert = require('assert')
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1)
+
+describe('Cart', function () {
+    describe('add product', function () {
+        it('should return product name when the request is suceeded', function () {
+            request.get(
+                'http://localhost:3000/add-to-cart/testName',
+                function (err, res, body) {
+                    expect(res.statusCode).to.equal(200)
+                    const productName = JSON.parse(res.body)
+                    assert(products.name, 'testName')
+                    done()
+                }
+            )
         })
     })
 })
